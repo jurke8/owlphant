45f146fe268237bef476aa68164a401f11448b50
jurke8 <jurke8@users.noreply.github.com>
2026-02-08 08:46:31 +0100

    Initial Commit

---
+++ Owlphant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Owlphant
+//
+//  Created by Ivan Juric on 08.02.2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Owlphant/OwlphantApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlphantApp.swift
+//  Owlphant
+//
+//  Created by Ivan Juric on 08.02.2026..
+//
+
+import SwiftUI
+
+@main
+struct OwlphantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
